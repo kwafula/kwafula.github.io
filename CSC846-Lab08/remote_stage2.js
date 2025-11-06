f238dfcf37319be28b8a134bc1d65498cb08a78c
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2025-11-06 09:55:05 -0500

    Create remote_stage2.js

---
+++ CSC846-Lab08/remote_stage2.js
@@ -0,0 +1,8 @@
+// This code will be executed by the local script
+WScript.Echo("Remote script executed successfully!");
+
+function remoteFunction() {
+    WScript.Echo("This function is from the remote script.");
+}
+
+remoteFunction();
