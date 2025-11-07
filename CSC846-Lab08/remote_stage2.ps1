dd2158c0476592777b9637e396af14ddcc0b9b5b
Kevin Wafula <95890992+kwafula@users.noreply.github.com>
2025-11-06 20:04:59 -0500

    Update remote_stage2.ps1

--- CSC846-Lab08/remote_stage2.ps1
+++ CSC846-Lab08/remote_stage2.ps1
@@ -1,2 +1,2 @@
 # Navigates to the specified URL in the user's default browser.
-Start-Process "https://dsu.edu/news/beacom.html"
+Start-Process -FilePath "iexplore.exe" "https://dsu.edu/news/beacom.html"
